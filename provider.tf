dd96325d33262bd07bdd1a99ed550de0707c7a89
Julio Cesar <contato.jcsalerno@gmail.com>
2023-10-17 19:07:07 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.21.0"
     }
+  }
+  backend "s3" {
+    bucket = "devopspro-bu"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
