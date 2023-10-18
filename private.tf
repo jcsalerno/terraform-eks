845eca74cf25cc9794c201255611f286e7f9ab88
Julio Cesar <contato.jcsalerno@gmail.com>
2023-10-18 20:42:36 -0300

    feat: route table and subnet

--- private.tf
+++ private.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "eks_subenet_privatec_1a" {
+resource "aws_subnet" "eks_subnet_private_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
   availability_zone = "${data.aws_region.current.name}a"
@@ -12,7 +12,7 @@ resource "aws_subnet" "eks_subenet_privatec_1a" {
   )
 }
 
-resource "aws_subnet" "eks_subenet_private_1b" {
+resource "aws_subnet" "eks_subnet_private_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
   availability_zone = "${data.aws_region.current.name}b"
@@ -24,4 +24,14 @@ resource "aws_subnet" "eks_subenet_private_1b" {
       "kubernetes.io/role/internal-elb" = 1
     }
   )
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_priv_1a" {
+  subnet_id      = aws_subnet.eks_subnet_private_1a.id
+  route_table_id = aws_route_table.eks_private_route_table_1a.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_priv_1b" {
+  subnet_id      = aws_subnet.eks_subnet_private_1b.id
+  route_table_id = aws_route_table.eks_private_route_table_1b.id
 }
