aec929f67d4744f92ec51fb0294b22602fe3057b
Julio Cesar <contato.jcsalerno@gmail.com>
2023-10-18 09:51:11 -0300

    feat: public route table

--- public.tf
+++ public.tf
@@ -28,4 +28,15 @@ resource "aws_subnet" "eks_subenet_public_1b" {
   )
 }
 
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subenet_public_1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subenet_public_1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+
 
